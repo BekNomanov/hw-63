1d1d9320144488262a55187af7343fe416c04d18
Bekbolot Nomanov <nomanovbekbolot7@gmail.com>
2024-02-09 17:46:55 +0600

    Добавил компонет контвкты

---
+++ src/components/Contacts/Contacts.tsx
@@ -0,0 +1,15 @@
+const Contacts = () => {
+  return (
+    <div>
+      <h2>Контакты</h2>
+      <p>Связатся со мной</p>
+      <ul className="list-group">
+        <li className="list-group-item">+996 224 88 20 02</li>
+        <li className="list-group-item">Telegram +996 224 88 20 02</li>
+        <li className="list-group-item">Instagram @nbk_bek</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Contacts;
